ef59f93466809bd40b4554382b1238059441d231
tranvantai2905 <taiho58@gmail.com>
2024-04-06 21:54:31 +0700

    feat(chat-box-main-page): add mock-api and loading spin affect

--- src/api/https-api.ts
+++ src/api/https-api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_REACT_APP_BASE_OPEN_AI,
   headers: {
     "Content-type": "application/json",
   },
--- src/layouts/components/chat-box/chat-message/index.tsx
+++ src/layouts/components/chat-box/chat-message/index.tsx
@@ -5,14 +5,16 @@ import { useChatBoxContext } from "../chat-context";
 import { useEffect, useMemo, useRef } from "react";
 import { OnlineUserTyp } from "@/pages/home-page/right-sidebar/_types";
 import React, { forwardRef } from "react";
+import Spiner from "@/pages/shared/component/spiner";
 
 interface ChatMessagesProps {
   user: OnlineUserTyp;
   messages: MessageTyp[];
+  isLoading: boolean;
 }
 
 const ChatMessages = forwardRef(
-  ({ user, messages }: ChatMessagesProps, ref) => {
+  ({ user, messages, isLoading }: ChatMessagesProps, ref) => {
     const messagesEndRef = useRef<null | HTMLDivElement>(null);
     const scrollToBottom = () => {
       messagesEndRef.current?.scrollIntoView({ behavior: "instant" });
@@ -22,28 +24,33 @@ const ChatMessages = forwardRef(
     }, [messages]);
 
     return (
-      <div
-        className="grid h-80 gap-4 overflow-y-scroll p-2 pb-0 pt-5"
-        ref={ref as React.RefObject<HTMLDivElement>}
-      >
-        {renderMessages(messages)}
-        <div ref={messagesEndRef as React.RefObject<HTMLDivElement>} />
+      <div className="relative">
+        <Spiner isLoading={isLoading} className="absolute left-0 right-0" />
+        <div
+          className={`grid h-80 gap-4 overflow-y-scroll p-2 pb-0 pt-5 ${isLoading ? "opacity-70" : "opacity-100"}`}
+          ref={ref as React.RefObject<HTMLDivElement>}
+        >
+          {renderMessages(messages)}
+          <div ref={messagesEndRef as React.RefObject<HTMLDivElement>} />
+        </div>
       </div>
     );
   },
 );
 
 const ChatMessagesHOC = () => {
-  const { user, messages } = useChatBoxContext();
+  const { user, messages, isLoading } = useChatBoxContext();
 
   const userMemo = useMemo(() => user, [user]);
   const messagesMemo = useMemo(() => messages, [messages]);
+  const isLoadingMemo = useMemo(() => isLoading, [isLoading]);
 
   return (
     <>
       <ChatMessages
         user={userMemo as OnlineUserTyp}
         messages={messagesMemo as MessageTyp[]}
+        isLoading={isLoadingMemo as boolean}
       />
     </>
   );
